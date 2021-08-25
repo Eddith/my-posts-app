8e57cdf875c0209a074ae4e65a36214f81ac54fc
Ali Koçman <34556282+Eddith@users.noreply.github.com>
2021-08-25 11:07:52 +0300

    bio added

--- src/components/Bio/Bio.stories.js
+++ src/components/Bio/Bio.stories.js
@@ -8,7 +8,12 @@ export default {
 };
 
 const Template = () => (
-  <Bio />
+  <Bio 
+    headshot="https://avatars.githubusercontent.com/u/34556282?s=400&u=143676d757968aeb5e5135326963bb0d8265bec6&v=4"
+    name="Ali Koçman"
+    tagline="Helping others to learn by doing!"
+    role="Developer Advocate @ Applitools"
+  />
 );
 
 export const Default = Template.bind({});
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 import Post from "../components/Post";
 import PostForm from "../components/PostForm";
+import Bio from "../components/Bio"
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
@@ -14,7 +15,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>My Posts</h1>
+        <Bio 
+          headshot="https://avatars.githubusercontent.com/u/34556282?s=400&u=143676d757968aeb5e5135326963bb0d8265bec6&v=4"
+          name="Ali Koçman"
+          tagline="Helping others to learn by doing!"
+          role="Developer Advocate @ Applitools"
+        />
 
         <ul className={styles.posts}>
           <li>
